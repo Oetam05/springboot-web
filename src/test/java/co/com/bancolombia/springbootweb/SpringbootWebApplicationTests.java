055ae76bfefd5de3ac8d0568e7a08cd9903fc3ec
matar <matar@bancolombia.com.co>
2025-07-27 15:52:24 -0500

    initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/test/java/co/com/bancolombia/springbootweb/SpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.bancolombia.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
