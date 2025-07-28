d96699b6df70d7cdeee093a2a2fe930df20e9e57
matar <matar@bancolombia.com.co>
2025-07-27 21:06:20 -0500

    add configuration properties and new endpoints for user management

--- src/main/java/co/com/bancolombia/springbootweb/controllers/PathVariableController.java
+++ src/main/java/co/com/bancolombia/springbootweb/controllers/PathVariableController.java
@@ -1,14 +1,28 @@
 package co.com.bancolombia.springbootweb.controllers;
 
+import co.com.bancolombia.springbootweb.models.User;
 import co.com.bancolombia.springbootweb.models.dto.ParamDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
 
     @GetMapping("/baz/{message}/{code}")
     public ParamDto baz(@PathVariable String message,
@@ -18,4 +32,21 @@ public class PathVariableController {
         paramDto.setCode(code);
         return paramDto;
     }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values() {
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        json.put("code", code);
+        return json;
+    }
+
 }
